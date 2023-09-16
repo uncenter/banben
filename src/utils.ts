bc6cbceb926f889f937f728c106717d6a1381364
uncenter <47499684+uncenter@users.noreply.github.com>
2023-09-16 15:25:45 -0400

    fix: check if package.json exists

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,20 @@
 import detectIndent, { type Indent } from 'detect-indent';
-import { readFile, writeFile } from 'fs/promises';
+import { readFile, stat, writeFile } from 'fs/promises';
+import { log } from './bin';
 import type { JsonObject } from './types';
+
+export async function exists(path) {
+	try {
+		await stat(path);
+		return true;
+	} catch {
+		return false;
+	}
+}
 
 export class Package {
 	path: string;
 	json: JsonObject;
-
 	indent: Indent['indent'];
 	eofChar: string;
 
@@ -16,7 +25,9 @@ export class Package {
 	async read() {
 		if (this.json) return this.json;
 
+		if (!(await exists(this.path))) log.error('No package.json found!');
 		const file = await readFile(this.path, 'utf-8');
+
 		this.indent = detectIndent(file).indent;
 		this.eofChar = (file.match(/\r?\n$/) || [])[0] || '';
 		this.json = JSON.parse(file);
